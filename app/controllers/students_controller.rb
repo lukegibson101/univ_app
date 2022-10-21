0b482857c0d716b08c30ace00f5ab501a96521e0
lukegibson101 <luke.gibson@nourishcare.co.uk>
2022-10-21 11:00:01 +0100

    set students profile

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,8 +4,41 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
   def new
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You have successfully signed up"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You have successfully updated your profile"
+      redirect_to @student
+    else 
+      render 'edit'
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+
+  private
+  def student_params
+    params.require(:student).permit(:name, :email)
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,5 @@ ActiveStorage.start()
 $( document ).on('turbolinks:load', function() {
   $(".dropdown-trigger").dropdown();
   $('.sidenav').sidenav();
+  $('#fade-out-target').fadeOut(4000);
 })
